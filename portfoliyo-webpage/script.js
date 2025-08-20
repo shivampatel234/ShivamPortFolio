c189b71baf6d295107ba03a75a0f28b30fdb84bf
SHIVAM PATEL <129602334+shivampatel234@users.noreply.github.com>
2025-08-20 16:01:19 +0530

    Add files via upload

---
+++ portfoliyo-webpage/script.js
@@ -0,0 +1,24 @@
+let nav_item = document.querySelector(".show");
+console.log(nav_item)
+let Toggle = document.querySelector("#toggle");
+
+Toggle.addEventListener("click",(e)=>{
+    e.preventDefault()
+    nav_item.classList.toggle("hide")
+
+})
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
